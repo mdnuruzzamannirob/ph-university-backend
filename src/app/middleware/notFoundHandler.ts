0949c272086b6521a3297236a9c9e7d604cd5a5e
mdnuruzzamannirob <nuruzzamanmd2002@gmail.com>
2024-06-07 14:32:26 +0600

    added unhandled and uncaught error handling functionalities

--- src/app/middleware/notFoundHandler.ts
+++ src/app/middleware/notFoundHandler.ts
@@ -1,11 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
+import { TErrorSources } from '../interface/error';
 
 const notFoundHandler = (req: Request, res: Response, next: NextFunction) => {
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'API Not Found !',
+    },
+  ];
+
   return res.status(httpStatus.NOT_FOUND).json({
     success: false,
     message: 'API Not Found !',
-    error: '',
+    errorSources,
+    stack: null,
   });
 };
 
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
+import { Server } from 'http';
 import app from './app';
 import config from './app/config';
 import mongoose from 'mongoose';
+
+let server: Server;
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
 
-    app.listen(config.port, () => {
+    server = app.listen(config.port, () => {
       console.log(`App listening on port ${config.port}`);
     });
   } catch (err) {
@@ -14,3 +17,19 @@ async function main() {
   }
 }
 main();
+
+// unhandled Rejection error handling
+process.on('unhandledRejection', () => {
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
+
+// uncaught Exception error handling
+process.on('uncaughtException', () => {
+  process.exit(1);
+});
